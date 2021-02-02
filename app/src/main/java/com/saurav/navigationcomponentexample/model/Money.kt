6d788da39735b62b32f9a2830256103d6bb888a5
sauravjaiswa <41826172+sauravjaiswa@users.noreply.github.com>
2021-02-03 03:00:32 +0530

    Practicing Navigation Component

---
+++ app/src/main/java/com/saurav/navigationcomponentexample/model/Money.kt
@@ -0,0 +1,10 @@
+package com.saurav.navigationcomponentexample.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import java.math.BigDecimal
+
+@Parcelize
+data class Money(val amount: BigDecimal) : Parcelable {
+
+}
